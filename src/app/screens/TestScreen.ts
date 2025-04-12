b236cf35ff908ee07a4788e55084f73a5d69d1db
Charlie Davis <34718612+charliecarrot@users.noreply.github.com>
2025-04-12 10:15:17 -0600

    Add custom sprite for player

--- src/app/screens/TestScreen.ts
+++ src/app/screens/TestScreen.ts
@@ -1,67 +1,158 @@
-import { Container, Graphics, Ticker } from 'pixi.js';
+import {
+  AnimatedSprite,
+  Container,
+  Graphics,
+  Spritesheet,
+  Texture,
+  TexturePool,
+} from 'pixi.js';
 import { engine } from '../getEngine';
 import { PausePopup } from '../popups/PausePopup';
 
+const heroSheet = {
+  frames: {
+    idle: {
+      frame: { x: 0, y: 0, w: 64, h: 64 },
+      sourceSize: { w: 64, h: 64 },
+      spriteSourceSize: { x: 0, y: 0, w: 64, h: 64 },
+    },
+    walk: {
+      frame: { x: 64, y: 64, w: 128, h: 128 },
+      sourceSize: { w: 64, h: 64 },
+      spriteSourceSize: { x: 0, y: 0, w: 64, h: 64 },
+    },
+    jump: {
+      frame: { x: 128, y: 128, w: 192, h: 192 },
+      sourceSize: { w: 64, h: 64 },
+      spriteSourceSize: { x: 0, y: 0, w: 64, h: 64 },
+    },
+    fall: {
+      frame: { x: 192, y: 192, w: 256, h: 256 },
+      sourceSize: { w: 64, h: 64 },
+      spriteSourceSize: { x: 0, y: 0, w: 64, h: 64 },
+    },
+  },
+  meta: {
+    image: 'hero-spritesheet.png',
+    format: 'RGBA8888',
+    size: { w: 256, h: 64 },
+    scale: 1,
+    anchor: 0,
+  },
+  animations: {
+    idle: ['idle'],
+    walk: ['walk', 'idle'],
+    jump: ['jump', 'fall'],
+  },
+};
+
+//TODO: Maybe player isn't animated sprite, but it has an animated sprite that it adds to container?
+class Player extends Container {
+  private currentAnimation: AnimatedSprite;
+  private idleAnimation: AnimatedSprite;
+  private walkingAnimation: AnimatedSprite;
+  private jumpingAnimation: AnimatedSprite;
+
+  constructor() {
+    super();
+    // Create sprites from spritesheet
+    const sheet = new Spritesheet(
+      Texture.from(heroSheet.meta.image),
+      heroSheet
+    );
+    sheet.parse();
+    this.idleAnimation = new AnimatedSprite(sheet.animations.idle);
+    this.walkingAnimation = new AnimatedSprite(sheet.animations.walk);
+    this.jumpingAnimation = new AnimatedSprite(sheet.animations.jump);
+    this.currentAnimation = this.idleAnimation;
+    this.currentAnimation.play();
+  }
+
+  public pause() {
+    this.currentAnimation.stop();
+  }
+
+  public resume() {
+    this.currentAnimation.play();
+  }
+
+  private jump() {
+    //this.set
+  }
+
+  public async show(screen: TestScreen): Promise<void> {
+    screen.testContainer.addChild(this.currentAnimation);
+  }
+}
+
 export class TestScreen extends Container {
-    /**
-     * Asset bundles required for this screen, using AssetPack.
-     * The engine automatically loads the assets specified, based
-     * on the content of the `~/manifest.json` file.
-     *
-     * See more here: https://pixijs.io/create-pixi/docs/guide/creations/engine/#asset-loading
-     */
-    public static assetBundles = ['main']; // using main for now as placeholder (see MainScreen.ts)
+  /**
+   * Asset bundles required for this screen, using AssetPack.
+   * The engine automatically loads the assets specified, based
+   * on the content of `raw-assets/` folder.
+   *
+   * See more here: https://pixijs.io/create-pixi/docs/guide/creations/engine/#asset-loading
+   */
+  public static assetBundles = ['main', 'test']; // using main for now as placeholder (see MainScreen.ts)
 
-    public testContainer: Container;
+  public testContainer: Container;
+  private player: Player;
 
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.testContainer = new Container();
-        this.addChild(this.testContainer);
+    TexturePool.textureOptions.scaleMode = 'linear';
+
+    this.testContainer = new Container();
+    this.addChild(this.testContainer);
+
+    this.player = new Player();
+  }
+
+  /** Show the screen */
+  public async show() {
+    const shape = new Graphics()
+      .rect(200, 200, 200, 180)
+      .fill({ color: '#FFEA00', alpha: 0.8 });
+
+    this.testContainer.addChild(shape);
+    this.player.show(this);
+  }
+
+  /** Hide the screen */
+  //hide?(): Promise<void>;
+
+  /** Pause the screen */
+  public async pause() {
+    this.testContainer.interactiveChildren = false;
+    this.player.pause();
+  }
+
+  /** Resume the screen */
+  public async resume() {
+    this.testContainer.interactiveChildren = true;
+    this.player.resume();
+  }
+
+  /** Prepare screen, before showing */
+  //prepare?(): void;
+
+  /** Reset screen, after hidden */
+  //reset?(): void;
+
+  /** Update the screen, passing delta time/step */
+  //update?(time: Ticker): void;
+
+  /** Resize the screen */
+  //resize?(width: number, height: number): void;
+
+  /** Pause the app if the window loses focus */
+  public blur() {
+    if (!engine().navigation.currentPopup) {
+      engine().navigation.presentPopup(PausePopup);
     }
+  }
 
-    /** Show the screen */
-    public async show() {
-        const shape = new Graphics()
-            .rect(200, 200, 200, 180)
-            .fill({ color: '#FFEA00', alpha: 0.8 });
-
-        this.testContainer.addChild(shape);
-    }
-
-    /** Hide the screen */
-    //hide?(): Promise<void>;
-
-    /** Pause the screen */
-    public async pause() {
-        this.testContainer.interactiveChildren = false;
-    }
-
-    /** Resume the screen */
-    public async resume() {
-        this.testContainer.interactiveChildren = true;
-    }
-
-    /** Prepare screen, before showing */
-    //prepare?(): void;
-
-    /** Reset screen, after hidden */
-    //reset?(): void;
-
-    /** Update the screen, passing delta time/step */
-    update?(time: Ticker): void;
-
-    /** Resize the screen */
-    //resize?(width: number, height: number): void;
-
-    /** Pause the app if the window loses focus */
-    public blur() {
-        if (!engine().navigation.currentPopup) {
-            engine().navigation.presentPopup(PausePopup);
-        }
-    }
-
-    /** Focus the screen */
-    //focus?(): void;
+  /** Focus the screen */
+  //focus?(): void;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import { setEngine } from "./app/getEngine";
-import { LoadScreen } from "./app/screens/LoadScreen";
-import { TestScreen } from "./app/screens/TestScreen";
-import { userSettings } from "./app/utils/userSettings";
-import { CreationEngine } from "./engine/engine";
+import { setEngine } from './app/getEngine';
+import { LoadScreen } from './app/screens/LoadScreen';
+import { TestScreen } from './app/screens/TestScreen';
+import { userSettings } from './app/utils/userSettings';
+import { CreationEngine } from './engine/engine';
 
 /**
  * Importing these modules will automatically register there plugins with the engine.
  */
-import "@pixi/sound";
+import '@pixi/sound';
 // import "@esotericsoftware/spine-pixi-v8";
 
 // Create a new creation engine instance
@@ -17,9 +17,14 @@ setEngine(engine);
 (async () => {
   // Initialize the creation engine instance
   await engine.init({
-    background: "#1E1E1E",
+    background: '#1E1E1E',
     resizeOptions: { minWidth: 768, minHeight: 1024, letterbox: false },
   });
+
+  // @ts-expect-error used for dev tools
+  globalThis.__PIXI_STAGE__ = engine.stage;
+  // @ts-expect-error used for dev tools
+  globalThis.__PIXI_RENDERER__ = engine.renderer;
 
   // Initialize the user settings
   userSettings.init();
