8edbe4d3541b4dafe1e7b371dc49d007a456bb1a
Charlie Davis <34718612+charliecarrot@users.noreply.github.com>
2025-04-12 12:30:16 -0600

    Update eslint rules to ignore vars starting with underscore and auto add type imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,29 @@
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
-    rules: {},
-  },
+    rules: {
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        {
+          prefer: 'type-imports',
+          fixStyle: 'inline-type-imports',
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  }
 );
